0fcc55664b1b8319262950ed8c49bcb5cf430c6d
Simon Wörpel <simon.woerpel@pm.me>
2023-06-14 20:44:05 +0200

    🩹🧪 (util) try to fix test for py3.10

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,6 +1,6 @@
 import sys
 
-if sys.version_info > (3, 10):
+if sys.version_info >= (3, 11):
     from enum import EnumType
 
 from nomenklatura.dataset import Dataset
@@ -23,5 +23,5 @@ def test_util_str_enum():
     enum = util.StrEnum("Foo", ["a", "b", 2])
     assert enum.a == "a"
     assert str(enum.a) == "a"
-    if sys.version_info > (3, 10):
+    if sys.version_info >= (3, 11):
         assert isinstance(enum, EnumType)
