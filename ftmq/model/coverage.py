9a762f2538059ee9467a578a868599cd13ed083d
Simon Wörpel <simon.woerpel@pm.me>
2023-07-30 18:06:21 +0200

    ✨ (model) coverage apply proxy stream

--- ftmq/model/coverage.py
+++ ftmq/model/coverage.py
@@ -6,7 +6,7 @@ from nomenklatura.dataset.coverage import DataCoverage as NKCoverage
 from pydantic import PrivateAttr
 
 from ftmq.enums import Properties
-from ftmq.types import CE, Frequencies, Schemata
+from ftmq.types import CE, CEGenerator, Frequencies, Schemata
 
 from .mixins import NKModel
 
@@ -31,18 +31,28 @@ class Collector:
         self.end.update(proxy.get(Properties.endDate, quiet=True))
         self.end.update(proxy.get(Properties.date, quiet=True))
 
-    def close(self) -> "Coverage":
+    def export(self) -> "Coverage":
         return Coverage(
-            start=min(self.start),
-            end=max(self.end),
+            start=min(self.start) if self.start else None,
+            end=max(self.end) if self.end else None,
             schemata=dict(self.schemata),
             countries=self.countries,
             entities=self.schemata.total(),
         )
 
     def to_dict(self) -> dict[str, Any]:
-        data = self.close()
+        data = self.export()
         return data.dict()
+
+    @classmethod
+    def apply(cls, proxies: CEGenerator) -> "Coverage":
+        """
+        Generate coverage from an input stream of proxies
+        """
+        cov = cls()
+        for proxy in proxies:
+            cov.collect(proxy)
+        return cov.export()
 
 
 class Coverage(NKModel):
@@ -63,7 +73,7 @@ class Coverage(NKModel):
         return self._collector
 
     def __exit__(self, *args, **kwargs):
-        res = self._collector.close()
+        res = self._collector.export()
         self._collector = None
         self.start = res.start
         self.end = res.end
