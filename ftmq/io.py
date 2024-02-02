b6f2ae72c1c8ce7de0d520cc6b4c7bb5edb0784e
Simon Wörpel <simon.woerpel@pm.me>
2024-02-02 12:06:22 +0100

    👽️ (io) move things upstream to anystore

--- ftmq/io.py
+++ ftmq/io.py
@@ -1,106 +1,19 @@
-import contextlib
 import logging
-import sys
 from collections.abc import Iterable
-from typing import Any, Literal
 
 import orjson
+from anystore.io import smart_open, smart_stream
 from banal import is_listish
-from fsspec import open
 from nomenklatura.entity import CE, CompositeEntity
 from nomenklatura.util import PathLike
 
 from ftmq.query import Query
-from ftmq.store import Store, get_store
 from ftmq.types import CEGenerator, SDict
 from ftmq.util import get_statements, make_dataset, make_proxy
 
 log = logging.getLogger(__name__)
 
 DEFAULT_MODE = "rb"
-
-
-def _get_sysio(mode: str | None = DEFAULT_MODE) -> Literal[sys.stdin, sys.stdout]:
-    if mode.startswith("r"):
-        return sys.stdin
-    return sys.stdout
-
-
-class SmartHandler:
-    def __init__(
-        self,
-        uri: Any,
-        *args,
-        **kwargs,
-    ) -> None:
-        if not uri:
-            raise ValueError("Missing uri")
-        self.uri = str(uri)
-        self.args = args
-        kwargs["mode"] = kwargs.get("mode", DEFAULT_MODE)
-        self.sys_io = _get_sysio(kwargs["mode"])
-        if kwargs["mode"].endswith("b"):
-            self.sys_io = self.sys_io.buffer
-        self.kwargs = kwargs
-        self.is_buffer = self.uri == "-"
-        self.handler = None
-
-    def open(self):
-        if self.is_buffer:
-            self.handler = self.sys_io
-        else:
-            handler = open(self.uri, *self.args, **self.kwargs)
-            self.handler = handler.open()
-        return self.handler
-
-    def close(self):
-        if not self.is_buffer:
-            self.handler.close()
-
-    def __enter__(self):
-        return self.open()
-
-    def __exit__(self, *args, **kwargs) -> None:
-        self.close()
-
-
-@contextlib.contextmanager
-def smart_open(
-    uri: Any,
-    mode: str | None = None,
-    *args,
-    **kwargs,
-):
-    if mode is not None:
-        kwargs["mode"] = mode
-    else:
-        kwargs["mode"] = kwargs.get("mode", DEFAULT_MODE)
-    handler = SmartHandler(uri, *args, **kwargs)
-    try:
-        yield handler.open()
-    finally:
-        handler.close()
-
-
-def _smart_stream(uri, *args, **kwargs) -> Any:
-    with smart_open(uri, *args, **kwargs) as fh:
-        while line := fh.readline():
-            yield line
-
-
-def smart_read(uri, *args, **kwargs) -> Any:
-    stream = kwargs.pop("stream", False)
-    if stream:
-        return _smart_stream(uri, *args, **kwargs)
-
-    with smart_open(uri, *args, **kwargs) as fh:
-        return fh.read()
-
-
-def smart_write(uri, content: bytes | str, *args, **kwargs) -> Any:
-    kwargs["mode"] = kwargs.get("mode", "wb")
-    with smart_open(uri, *args, **kwargs) as fh:
-        fh.write(content)
 
 
 def smart_get_store(uri: PathLike, **kwargs) -> Store | None:
@@ -128,7 +41,7 @@ def smart_read_proxies(
         yield from view.entities(query)
         return
 
-    lines = smart_read(uri, stream=True)
+    lines = smart_stream(uri)
     lines = (orjson.loads(line) for line in lines)
     if serialize or query:
         q = query or Query()
